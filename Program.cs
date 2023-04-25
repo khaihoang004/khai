13b12d6a64ccc9c46bbd655c9856c8b38c2630e8
khaihoang004 <130827999+khaihoang004@users.noreply.github.com>
2023-04-25 09:41:22 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using Maphuong;
+using System;
+using System.Numerics;
+
+namespace HBAcademy
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            asdasdsa:
+            Console.Write("Nhap kich co cua ma phuong (n * n): ");
+            int n = int.Parse(Console.ReadLine());
+            int[,] Maphuong = new int[n,n];
+           
+            if (n%2 != 0) 
+            {
+                Maphuong = GiaiMaphuong.Type1(Maphuong, n);
+            }
+            else if  (n%4 ==0)
+            {
+                Maphuong = GiaiMaphuong.Type2(Maphuong, n);
+            }
+            else
+            {
+                Maphuong = GiaiMaphuong.Type3(Maphuong, n);
+            }
+
+
+            Console.WriteLine("------------------------------------------------");
+            Console.Beep();
+            GiaiMaphuong.Print(Maphuong);
+            Console.WriteLine("------------------------------------------------");
+            
+            Console.WriteLine("Nhap lai? (Y)");
+            char x = char.Parse(Console.ReadLine());
+            if (x == 'y')
+            {
+                Console.Clear();
+                goto asdasdsa;
+            }
+            else
+            {
+                Console.WriteLine("Bandicut");
+            }
+        }
+    }
+}
